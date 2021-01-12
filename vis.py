49be0fe31628d79b54716bf9d9f903abf28de173
amanda <amanda@zipcodes-MacBook-Pro.local>
2021-01-12 14:47:24 -0500

    Updated requirements, cleaned up vis.py

--- vis.py
+++ vis.py
@@ -18,12 +18,22 @@ def connect_sql():
     return engine
 
 def get_state_data():
+    """
+    Gets merchant and revenue data from SQL for Delaware
+
+    :return: Dataframes of merchant and revenue data for the state
+    """
     con = connect_sql()
     de_merch = pd.read_sql('select date, merchants_all from state_merch where statefips = 10;',con)
     de_rev = pd.read_sql('select date, revenue_all from state_rev where statefips = 10', con)
     return de_merch, de_rev
 
 def get_county_merch():
+    """
+    Gets merchant data from SQL for each Delaware county
+
+    :return: A dataframe for each county
+    """
     con = connect_sql()
     Kent_m = pd.read_sql('select date, merchants_all from county_merch where countyfips = 10001;', con)
     NCC_m = pd.read_sql('select date, merchants_all from county_merch where countyfips = 10003;', con)
@@ -31,6 +41,11 @@ def get_county_merch():
     return Kent_m, NCC_m, Sussex_m
 
 def get_county_rev():
+    """
+    Gets revenue data from SQL for each Delaware county
+
+    :return: A dataframe for each county
+    """
     con = connect_sql()
     Kent_r = pd.read_sql('select date, revenue_all from county_rev where countyfips = 10001;', con)
     NCC_r = pd.read_sql('select date, revenue_all from county_rev where countyfips = 10003;', con)
@@ -38,6 +53,13 @@ def get_county_rev():
     return Kent_r, NCC_r, Sussex_r
 
 def get_restrictions():
+    """
+    Gets dataframe of restrictions. Creates of list of lists.
+
+
+    :return: A list of lists. First list is dates in ascending order.
+    The second list is the number of times the data appears.
+    """
     con = connect_sql()
     restrict = pd.read_sql('select * from mitigation;', con)
     restriction = [[],[]]
@@ -51,6 +73,13 @@ def get_restrictions():
     return restriction
 
 def graph_state_data(df1, df2):
+    """
+    Graphs to lines based on the dataframes.
+
+    :param df1: Dataframe
+    :param df2: Dataframe
+    :return: None
+    """
     merch, = plt.plot(df1['date'], df1['merchants_all']* 100)
     rev, = plt.plot(df2['date'], df2['revenue_all'] * 100)
     plt.xlabel('Date')
@@ -58,19 +87,19 @@ def graph_state_data(df1, df2):
     plt.legend([merch, rev], ['Merchant', 'Revenue'])
     plt.show()
 
-def graph_county_merch(Kent, NCC, Sussex):
-    K, = plt.plot(Kent['date'], Kent['merchants_all']*100, label='Kent County')
-    NC, = plt.plot(NCC['date'], NCC['merchants_all']*100, label='New Castle County')
-    S, = plt.plot(Sussex['date'], Sussex['merchants_all']*100, label='Sussex')
-    plt.xlabel('Date')
-    plt.xticks(rotation=45)
-    plt.legend([K, NC, S], ['Kent County', 'New Castle County', 'Sussex'])
-    plt.show()
-
 def graph_county(Kent, NCC, Sussex, column, restrictions):
-    K_r, = plt.plot(Kent['date'], Kent[column]*100)
-    NC_r, = plt.plot(NCC['date'], NCC[column]*100)
-    S_r, = plt.plot(Sussex['date'], Sussex[column]*100)
+    """
+
+    :param Kent: Dataframe
+    :param NCC: Dataframe
+    :param Sussex: Dataframe
+    :param column: column name either 'merchants' or 'revenue'
+    :param restrictions: Dataframe of restrictions
+    :return: None
+    """
+    K_r, = plt.plot(Kent['date'], Kent[str(column)+'_all']*100)
+    NC_r, = plt.plot(NCC['date'], NCC[str(column)+'_all']*100)
+    S_r, = plt.plot(Sussex['date'], Sussex[str(column)+'_all']*100)
     plt.xlabel('Date')
     plt.xticks(rotation=45)
     plt.legend([K_r, NC_r, S_r], ['Kent County', 'New Castle County', 'Sussex'], bbox_to_anchor=(1, 1), loc='upper left')
@@ -88,22 +117,6 @@ def graph_county(Kent, NCC, Sussex, column, restrictions):
         line = (pd.to_datetime(restrictions[0][i]), str_val)
         restrict_line.append(line)
 
-    """    
-    restrict = [
-        (pd.to_datetime("2020-03-12"), '1'),
-        (pd.to_datetime("2020-03-22"), '2'),
-        (pd.to_datetime("2020-03-29"), '3'),
-        (pd.to_datetime("2020-04-24"), '4'),
-        (pd.to_datetime("2020-04-28"), '5'),
-        (pd.to_datetime("2020-05-08"), '6'),
-        (pd.to_datetime("2020-05-12"), '7'),
-        (pd.to_datetime("2020-05-22"), '8, 9'),
-        (pd.to_datetime("2020-06-01"), '10, 11'),
-        (pd.to_datetime("2020-06-15"), '12'),
-        (pd.to_datetime("2020-06-20"), '13'),
-        (pd.to_datetime("2020-06-22"), '14')
-    ]
-    """
     ax = plt.gca()
 
     for date, label in restrict_line:
@@ -114,27 +127,7 @@ def graph_county(Kent, NCC, Sussex, column, restrictions):
                  verticalalignment='center',
                  rotation=90,
                  color='grey')
-
-    #datacursor(K_r)
     plt.show()
-"""
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.imshow(Kent['revenue_all'], cmap=cm.jet, interpolation='nearest')
-
-    numrows, numcols = X.shape
-
-    def format_coord(x, y):
-        col = int(x + 0.5)
-        row = int(y + 0.5)
-        if col >= 0 and col < numcols and row >= 0 and row < numrows:
-            z = X[row, col]
-            return 'x=%1.4f, y=%1.4f, z=%1.4f' % (x, y, z)
-        else:
-            return 'x=%1.4f, y=%1.4f' % (x, y)
-
-    ax.format_coord = format_coord
-"""
 
 restrictions = get_restrictions()
 
@@ -142,10 +135,10 @@ restrictions = get_restrictions()
 #graph_state_data(de_merch, de_rev)
 
 Kent_m, NCC_m, Sussex_m = get_county_merch()
-graph_county(Kent_m, NCC_m, Sussex_m, 'merchants_all', restrictions)
+graph_county(Kent_m, NCC_m, Sussex_m, 'merchants', restrictions)
 
 #Kent_r, NCC_r, Sussex_r = get_county_rev()
-#graph_county(Kent_r, NCC_r, Sussex_r, 'revenue_all', restrictions)
+#graph_county(Kent_r, NCC_r, Sussex_r, 'revenue', restrictions)
 
 
 
